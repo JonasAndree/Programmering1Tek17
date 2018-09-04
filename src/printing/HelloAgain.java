3d6024c0f13a548f4b0891cddd1a3ca43eef302e
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2018-09-04 08:43:38 +0200

    println print

---
+++ src/printing/HelloAgain.java
@@ -0,0 +1,10 @@
+package printing;
+
+public class HelloAgain {
+
+	public static void main (String[] args) {
+		System.out.print("Jonas");
+		System.out.println(" är eran \n \n \n \n \\ GUD!!!!");
+	}
+
+}
